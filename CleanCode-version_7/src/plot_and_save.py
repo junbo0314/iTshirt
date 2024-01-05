1a32a391b8c49a62085c77b7569fbcdbdf80d114
junbo0314 <junbo0314@g.skku.edu>
2024-01-05 14:41:56 +0900

    두 번째 커밋

--- CleanCode-version_7/src/plot_and_save.py
+++ CleanCode-version_7/src/plot_and_save.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
-def plot_final_weights(DIR_NAME, rec_weights, n_maps) :
 
+
+def plot_final_weights(DIR_NAME, rec_weights, n_maps, time) :
+    weight_mean=list()
+    for map in range(n_maps) :
+        weight_mean.append(np.mean(rec_weights[map]))
+    weight_mean_numpy=np.array(weight_mean)
+    np.save('figures/'+DIR_NAME+'weight_mean_numpy.npy',weight_mean_numpy)
 
     labels = ['Current Goal', 'Old Goal', 'Sub Goal', 'Free']
     colors = ['red','blue','green','purple']
